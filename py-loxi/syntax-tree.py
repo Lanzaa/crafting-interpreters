e0a1f5591f9a1994a71a3b17fafe46f8d058be55
Colin B <lanzaa@gmail.com>
2023-02-22 11:57:02 -0800

    Ch05 Add syntax-tree and init ast-printer

---
+++ py-loxi/syntax-tree.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+class Expr:
+  pass
+
+@dataclass
+class Binary(Expr):
+  left: Expr
+  operator: Token
+  right: Expr
+
+@dataclass
+class Grouping(Expr):
+  expression: Expr
+
+@dataclass
+class Literal(Expr):
+  value: Object
+
+@dataclass
+class Unary(Expr):
+  operator: Token
+  right: Expr
+
+
+def pattern_match_example(node: Expr):
+  match node:
+    case Binary(left: Expr, operator: Token, right: Expr):
+      pass
+    case Grouping(expression: Expr):
+      pass
+    case Literal(value: Object):
+      pass
+    case Unary(operator: Token, right: Expr):
+      pass
+    case _:
+      raise ValueError("Unknown type")
+
